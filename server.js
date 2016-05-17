bfc3852cc1ddcebc2246d3b933f327a180278497
Arto Santala <arto.santala@solita.fi>
2016-05-17 07:37:57 +0300

    Services are now dependant on eachothers.

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 var express = require("express");
 var app = express();
+var Client = require('node-rest-client').Client;
+var client = new Client();
+
   
 app.get("/", function(req, res) {
+	client.get("http://localhost:4000", function (data, response) {
+    	console.log(data.toString());
+	});
     res.send(new Date());
 });
  
 var server = app.listen(3000, function () {
     console.log("Listening on port %s...", server.address().port);
 });
+
+
